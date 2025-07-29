61f4635b170313a6bfac01ad72efd45a86684c8c
Christopher Kenaip <ckenaip@f1r8s3.clusters.42paris.fr>
2025-07-29 21:42:39 +0200

    c'est fun pour le moment

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -61,6 +61,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <signal.h>
 
 int	server_fd;
 
@@ -81,7 +82,10 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     
     // char *hello = "Hello from server";
-    
+	char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    // std::string	files = ;
+	// int			fd = open()
+
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
@@ -119,12 +123,70 @@ int main(int argc, char const *argv[])
         char buffer[30000] = {0};
         valread = read(new_socket , buffer, 30000);
         printf("%s\n",buffer );
-		for (int i = 1; argv[i]; i++) {
-			write(new_socket , argv[i] , strlen(argv[i]));
-			write(new_socket , "\n" , strlen("\n"));
-		}
+		write(new_socket , hello , strlen(hello));
         printf("------------------Hello message sent-------------------\n");
         close(new_socket);
     }
     return 0;
 }
+
+// #include <stdio.h>
+// #include <sys/socket.h>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <netinet/in.h>
+// #include <string.h>
+
+// #define PORT 8080
+// int main(int argc, char const *argv[])
+// {
+//     int server_fd, new_socket; long valread;
+//     struct sockaddr_in address;
+//     int addrlen = sizeof(address);
+    
+//     // Only this line has been changed. Everything is same.
+//     char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    
+//     // Creating socket file descriptor
+//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+//     {
+//         perror("In socket");
+//         exit(EXIT_FAILURE);
+//     }
+    
+
+//     address.sin_family = AF_INET;
+//     address.sin_addr.s_addr = INADDR_ANY;
+//     address.sin_port = htons( PORT );
+    
+//     memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+//     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+//     {
+//         perror("In bind");
+//         exit(EXIT_FAILURE);
+//     }
+//     if (listen(server_fd, 10) < 0)
+//     {
+//         perror("In listen");
+//         exit(EXIT_FAILURE);
+//     }
+//     while(1)
+//     {
+//         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+//         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+//         {
+//             perror("In accept");
+//             exit(EXIT_FAILURE);
+//         }
+        
+//         char buffer[30000] = {0};
+//         valread = read( new_socket , buffer, 30000);
+//         printf("%s\n",buffer );
+//         write(new_socket , hello , strlen(hello));
+//         printf("------------------Hello message sent-------------------");
+//         close(new_socket);
+//     }
+//     return 0;
+// }
